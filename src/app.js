98cfd28ba676b873d6c4355dbf6a8cb20abe2874
rodolfoteran79 <rodolfoteran79@hotmail.com>
2024-10-11 05:02:50 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,51 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
 };
+
+const palos = ["♠", "♣", "♥", "♦"];
+const valores = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+];
+
+function generarCartaAleatoria() {
+  const paloIndex = Math.floor(Math.random() * 4);
+  const valorIndex = Math.floor(Math.random() * 13);
+  return {
+    palo: palos[paloIndex],
+    valor: valores[valorIndex],
+    clase: ["spade", "club", "heart", "diamond"][paloIndex]
+  };
+}
+
+function mostrarCarta() {
+  const { palo, valor, clase } = generarCartaAleatoria();
+
+  document.querySelector("#app").innerHTML = `
+    <div class="card ${clase}">
+      <div class="card-corner top-left">
+        <div>${valor}</div>
+        <div>${palo}</div>
+      </div>
+      <div class="card-center">${palo}</div>
+      <div class="card-corner bottom-right">
+        <div>${valor}</div>
+        <div>${palo}</div>
+      </div>
+    </div>
+  `;
+}
+
+window.onload = mostrarCarta;
